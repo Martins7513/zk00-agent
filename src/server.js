b4ebe710bc9f6332d937a75a61d0fe74edbe1d2a
Martins7513 <martinsmilagres10@gmail.com>
2026-07-24 15:50:07 -0300

    Increase body size limits for JSON and URL-encoded data
    Increased JSON and URL-encoded body size limits to 50mb.

--- src/server.js
+++ src/server.js
@@ -184,7 +184,8 @@ process.on('SIGTERM', () => {
 process.on('SIGINT', () => process.emit('SIGTERM'));
 
 app.use(cors({ origin: '*', credentials: true }));
-app.use(express.json({ limit: '10mb' }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(express.static(path.join(__dirname, '../public')));
 
 // Rota mobile
